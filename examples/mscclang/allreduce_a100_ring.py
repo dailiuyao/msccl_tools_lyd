07a83fdbcdf24ceb828c1d45cf91c5f100c8fc7c
dailiuyao <ldai8@ucmerced.edu>
2023-10-24 18:25:19 -0700

    scratch xml

--- examples/mscclang/allreduce_a100_ring.py
+++ examples/mscclang/allreduce_a100_ring.py
@@ -16,58 +16,197 @@ def allreduce_ring(size, instances, channels, protocol):
     with MSCCLProgram(f"allreduce_ring_{channels}channelsperring", topology, collective, instances,
          protocol=protocol, threadblock_policy=ThreadblockPolicy.manual):
         # Reduce ring
-        for step in range(0, size-1):
+        ### channel 0-3
+        step = 0
+        for index in range(0, size):
+            rank = (index + step) % size
+            next_rank = (index + step + 1) % size
+            channel = index%channels
+            c = chunk(rank, Buffer.input, index)
+            c.copy(next_rank, 'scratch', index, ch=channel, recvtb=channel, sendtb=channel)
+                
+        for step in range(1, size-2):
             for index in range(0, size):
                 rank = (index + step) % size
                 next_rank = (index + step + 1) % size
                 channel = index%channels
-                c = chunk(next_rank, Buffer.input, index)
-                c.reduce(chunk(rank, Buffer.input, index), ch=channel, recvtb=channel, sendtb=channel)
+                c = chunk(rank, Buffer.input, index)
+                c.reduce(chunk(rank, 'scratch', index), ch=channel, recvtb=channel, sendtb=channel)
+                c1 = chunk(rank, Buffer.input, index)
+                c1.copy(next_rank, 'scratch', index, ch=channel, recvtb=channel, sendtb=channel)
         
-        for step in range(0, size-1):
-            gpu_index = [0, 1, 3, 2]
+        step = size-2
+        for index in range(0, size):
+            rank = (index + step) % size
+            next_rank = (index + step + 1) % size
+            channel = index%channels
+            c = chunk(rank, Buffer.input, index)
+            c.reduce(chunk(rank, 'scratch', index), ch=channel, recvtb=channel, sendtb=channel)
+            c1 = chunk(rank, Buffer.input, index)
+            c1.copy(next_rank, 'scratch', index, ch=channel, recvtb=channel, sendtb=channel)
+            c2 = chunk(next_rank, Buffer.input, index) 
+            c2.reduce(chunk(next_rank, 'scratch', index), ch=channel, recvtb=channel, sendtb=channel)
+        
+        # channel 4-7
+        gpu_index = [0, 1, 3, 2]
+        step = 0
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 4
+            c = chunk(rank, Buffer.input, index + 4)
+            c.copy(next_rank, 'scratch', index + 4, ch=channel, recvtb=channel, sendtb=channel)
+                
+        for step in range(1, size-2):
             for index in range(0, size):
                 rank = gpu_index[(index + step) % size]
                 next_rank = gpu_index[(index + step + 1) % size]
                 channel = index%channels + 4
-                c = chunk(next_rank, Buffer.input, index + 4)
-                c.reduce(chunk(rank, Buffer.input, index + 4), ch=channel, recvtb=channel, sendtb=channel)
+                c = chunk(rank, Buffer.input, index + 4)
+                c.reduce(chunk(rank, 'scratch', index + 4), ch=channel, recvtb=channel, sendtb=channel)
+                c1 = chunk(rank, Buffer.input, index + 4)
+                c1.copy(next_rank, 'scratch', index + 4, ch=channel, recvtb=channel, sendtb=channel)
+        
+        step = size-2
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 4
+            c = chunk(rank, Buffer.input, index + 4)
+            c.reduce(chunk(rank, 'scratch', index + 4), ch=channel, recvtb=channel, sendtb=channel)
+            c1 = chunk(rank, Buffer.input, index + 4)
+            c1.copy(next_rank, 'scratch', index + 4, ch=channel, recvtb=channel, sendtb=channel)
+            c2 = chunk(next_rank, Buffer.input, index + 4) 
+            c2.reduce(chunk(next_rank, 'scratch', index + 4), ch=channel, recvtb=channel, sendtb=channel)
+        
+        ### channel 8-11        
+        gpu_index = [0, 2, 3, 1]
+        step = 0
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 8
+            c = chunk(rank, Buffer.input, index + 8)
+            c.copy(next_rank, 'scratch', index + 8, ch=channel, recvtb=channel, sendtb=channel)
                 
-        for step in range(0, size-1):
-            gpu_index = [0, 2, 3, 1]
+        for step in range(1, size-2):
             for index in range(0, size):
                 rank = gpu_index[(index + step) % size]
                 next_rank = gpu_index[(index + step + 1) % size]
                 channel = index%channels + 8
-                c = chunk(next_rank, Buffer.input, index + 8)
-                c.reduce(chunk(rank, Buffer.input, index + 8), ch=channel, recvtb=channel, sendtb=channel)
+                c = chunk(rank, Buffer.input, index + 8)
+                c.reduce(chunk(rank, 'scratch', index + 8), ch=channel, recvtb=channel, sendtb=channel)
+                c1 = chunk(rank, Buffer.input, index + 8)
+                c1.copy(next_rank, 'scratch', index + 8, ch=channel, recvtb=channel, sendtb=channel)
+        
+        step = size-2
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 8
+            c = chunk(rank, Buffer.input, index + 8)
+            c.reduce(chunk(rank, 'scratch', index + 8), ch=channel, recvtb=channel, sendtb=channel)
+            c1 = chunk(rank, Buffer.input, index + 8)
+            c1.copy(next_rank, 'scratch', index + 8, ch=channel, recvtb=channel, sendtb=channel)
+            c2 = chunk(next_rank, Buffer.input, index + 8) 
+            c2.reduce(chunk(next_rank, 'scratch', index + 8), ch=channel, recvtb=channel, sendtb=channel)
+        
+        
+        # channel 12-15         
+        gpu_index = [0, 2, 1, 3]
+        step = 0
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 12
+            c = chunk(rank, Buffer.input, index + 12)
+            c.copy(next_rank, 'scratch', index + 12, ch=channel, recvtb=channel, sendtb=channel)
                 
-        for step in range(0, size-1):
-            gpu_index = [0, 2, 1, 3]
+        for step in range(1, size-2):
             for index in range(0, size):
                 rank = gpu_index[(index + step) % size]
                 next_rank = gpu_index[(index + step + 1) % size]
                 channel = index%channels + 12
-                c = chunk(next_rank, Buffer.input, index + 12)
-                c.reduce(chunk(rank, Buffer.input, index + 12), ch=channel, recvtb=channel, sendtb=channel)
+                c = chunk(rank, Buffer.input, index + 12)
+                c.reduce(chunk(rank, 'scratch', index + 12), ch=channel, recvtb=channel, sendtb=channel)
+                c1 = chunk(rank, Buffer.input, index + 12)
+                c1.copy(next_rank, 'scratch', index + 12, ch=channel, recvtb=channel, sendtb=channel)
+        
+        step = size-2
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 12
+            c = chunk(rank, Buffer.input, index + 12)
+            c.reduce(chunk(rank, 'scratch', index + 12), ch=channel, recvtb=channel, sendtb=channel)
+            c1 = chunk(rank, Buffer.input, index + 12)
+            c1.copy(next_rank, 'scratch', index + 12, ch=channel, recvtb=channel, sendtb=channel)
+            c2 = chunk(next_rank, Buffer.input, index + 12) 
+            c2.reduce(chunk(next_rank, 'scratch', index + 12), ch=channel, recvtb=channel, sendtb=channel)
+        
+        # channel 16-19              
+        gpu_index = [0, 3, 1, 2]
+        step = 0
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 16
+            c = chunk(rank, Buffer.input, index + 16)
+            c.copy(next_rank, 'scratch', index + 16, ch=channel, recvtb=channel, sendtb=channel)
                 
-        for step in range(0, size-1):
-            gpu_index = [0, 3, 1, 2]
+        for step in range(1, size-2):
             for index in range(0, size):
                 rank = gpu_index[(index + step) % size]
                 next_rank = gpu_index[(index + step + 1) % size]
                 channel = index%channels + 16
-                c = chunk(next_rank, Buffer.input, index + 16)
-                c.reduce(chunk(rank, Buffer.input, index + 16), ch=channel, recvtb=channel, sendtb=channel)
+                c = chunk(rank, Buffer.input, index + 16)
+                c.reduce(chunk(rank, 'scratch', index + 16), ch=channel, recvtb=channel, sendtb=channel)
+                c1 = chunk(rank, Buffer.input, index + 16)
+                c1.copy(next_rank, 'scratch', index + 16, ch=channel, recvtb=channel, sendtb=channel)
         
-        for step in range(0, size-1):
-            gpu_index = [0, 3, 2, 1]
+        step = size-2
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 16
+            c = chunk(rank, Buffer.input, index + 16)
+            c.reduce(chunk(rank, 'scratch', index + 16), ch=channel, recvtb=channel, sendtb=channel)
+            c1 = chunk(rank, Buffer.input, index + 16)
+            c1.copy(next_rank, 'scratch', index + 16, ch=channel, recvtb=channel, sendtb=channel)
+            c2 = chunk(next_rank, Buffer.input, index + 16) 
+            c2.reduce(chunk(next_rank, 'scratch', index + 16), ch=channel, recvtb=channel, sendtb=channel)
+        
+        # channel 20-23
+        gpu_index = [0, 3, 2, 1]
+        step = 0
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 20
+            c = chunk(rank, Buffer.input, index + 20)
+            c.copy(next_rank, 'scratch', index + 20, ch=channel, recvtb=channel, sendtb=channel)
+                
+        for step in range(1, size-2):
             for index in range(0, size):
                 rank = gpu_index[(index + step) % size]
                 next_rank = gpu_index[(index + step + 1) % size]
                 channel = index%channels + 20
-                c = chunk(next_rank, Buffer.input, index + 20)
-                c.reduce(chunk(rank, Buffer.input, index + 20), ch=channel, recvtb=channel, sendtb=channel)
+                c = chunk(rank, Buffer.input, index + 20)
+                c.reduce(chunk(rank, 'scratch', index + 20), ch=channel, recvtb=channel, sendtb=channel)
+                c1 = chunk(rank, Buffer.input, index + 20)
+                c1.copy(next_rank, 'scratch', index + 20, ch=channel, recvtb=channel, sendtb=channel)
+        
+        step = size-2
+        for index in range(0, size):
+            rank = gpu_index[(index + step) % size]
+            next_rank = gpu_index[(index + step + 1) % size]
+            channel = index%channels + 20
+            c = chunk(rank, Buffer.input, index + 20)
+            c.reduce(chunk(rank, 'scratch', index + 20), ch=channel, recvtb=channel, sendtb=channel)
+            c1 = chunk(rank, Buffer.input, index + 20)
+            c1.copy(next_rank, 'scratch', index + 20, ch=channel, recvtb=channel, sendtb=channel)
+            c2 = chunk(next_rank, Buffer.input, index + 20) 
+            c2.reduce(chunk(next_rank, 'scratch', index + 20), ch=channel, recvtb=channel, sendtb=channel)
                 
         # Propagate ring
         for step in range(-1, size-2):
