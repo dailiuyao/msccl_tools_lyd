c10b631cbfeba1e2b6995a351b4468dea3c1b7b3
dailiuyao <ldai8@ucmerced.edu>
2023-10-25 10:32:56 -0700

    1024-2

--- examples/scripts/yuke_polaris_msccl_test.sh
+++ examples/scripts/yuke_polaris_msccl_test.sh
@@ -6,9 +6,9 @@
 #PBS -l filesystems=home
 #PBS -A CSC250STPM09
 #PBS -k doe
-#PBS -N nccl-tests-msccl-1024-1
-#PBS -o nccl-tests-msccl-1024-1.out
-#PBS -e nccl-tests-msccl-1024-1.error
+#PBS -N nccl-tests-msccl-1024-2
+#PBS -o nccl-tests-msccl-1024-2.out
+#PBS -e nccl-tests-msccl-1024-2.error
 
 set -x
 
@@ -1223,19 +1223,19 @@ MSCCL_TOOLS_SRC_LOCATION="/home/yuke/ncclPG/msccl_tools_lyd"
 
 # echo "MSCCL TEST BINARY-H-P-16chunk-16:8-2nicPtree INSTANCE: 1 CHANNEL: 24 end time: $(date)"
 
-echo "######################### LIBRARY: MSCCL ALGORITHM: A100-RING INSTANCE: 1 CHANNEL: 24 PROTOCOL: Simple ##############################################"
+echo "######################### LIBRARY: MSCCL ALGORITHM: A100-RING-2-Stage INSTANCE: 1 CHANNEL: 12 PROTOCOL: Simple ##############################################"
 
 # Print the current time
-echo "MSCCL TEST A100-RING INSTANCE: 1 CHANNEL: 24 start time: $(date)"
+echo "MSCCL TEST A100-RING-2-Stage INSTANCE: 1 CHANNEL: 12 start time: $(date)"
 
 
 export LD_LIBRARY_PATH=${MSCCL_SRC_LOCATION}/build/lib/:$LD_LIBRARY_PATH
 export NCCL_DEBUG=TRACE
 export NCCL_DEBUG_SUBSYS=INIT,ENV
-export MSCCL_XML_FILES=${MSCCL_TOOLS_SRC_LOCATION}/examples/xml/allreduce_a100_ring_ch24_manul.xml
+export MSCCL_XML_FILES=${MSCCL_TOOLS_SRC_LOCATION}/examples/xml/allreduce_a100_ring_ch24_manul_2_stage.xml
 export NCCL_ALGO=MSCCL,TREE,RING
 export NCCL_PROTO=Simple
 
 mpiexec -n 4 --ppn 4 --cpu-bind core ${NCCL_TEST_HOME}/build/all_reduce_perf -b 96 -e 96MB -f 2 -g 1
 
-echo "MSCCL TEST A100-RING INSTANCE: 1 CHANNEL: 24 end time: $(date)"
+echo "MSCCL TEST A100-RING-2-Stage INSTANCE: 1 CHANNEL: 12 end time: $(date)"
