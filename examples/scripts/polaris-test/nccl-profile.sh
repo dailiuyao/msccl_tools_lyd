7ea0dbb24f7462b8cf9c4690c44d01dca1349729
dailiuyao <ldai8@ucmerced.edu>
2023-12-05 17:01:44 -0800

    add protocol=simple in nccl.profile.sh

--- examples/scripts/polaris-test/nccl-profile.sh
+++ examples/scripts/polaris-test/nccl-profile.sh
@@ -32,6 +32,7 @@ export LD_LIBRARY_PATH=${NCCL_SRC_LOCATION}/build/lib/:$LD_LIBRARY_PATH
 export NCCL_DEBUG=INFO
 export NCCL_DEBUG_SUBSYS=INIT,ENV
 export NCCL_ALGO=Tree
+export NCCL_PROTO=Simple
 
 
 mpiexec -n 2 --ppn 1 --cpu-bind core ${NCCL_TEST_HOME}/build/all_reduce_perf -b 2 -e 512MB -w 0 -n 1 -f 2 -g 1
