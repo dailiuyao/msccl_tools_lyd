e1d69676598619d4ea529bffb244919dcb7cba03
dailiuyao <ldai8@ucmerced.edu>
2023-12-12 22:42:33 -0800

    run plugin sh is nccl-profile.sh

--- examples/scripts/polaris-test/nccl-profile.sh
+++ examples/scripts/polaris-test/nccl-profile.sh
@@ -6,21 +6,18 @@
 #PBS -l filesystems=home
 #PBS -A CSC250STPM09
 #PBS -k doe
-#PBS -N nccl-profile-1210-2
-#PBS -o nccl-profile-1210-2.out
-#PBS -e nccl-profile-1210-2.error
+#PBS -N nccl-profile-plugin
+#PBS -o nccl-profile-plugin.out
+#PBS -e nccl-profile-plugin.error
 
 
-# module load gcc/11.2.0
-# module load cudatoolkit-standalone/11.4.4
-# export MPI_HOME=/opt/cray/pe/mpich/8.1.16/ofi/gnu/9.1
-# export CUDA_HOME=/soft/compilers/cudatoolkit/cuda-11.4.4/
+module load gcc/11.2.0
+module load cudatoolkit-standalone/11.4.4
+export MPI_HOME=/opt/cray/pe/mpich/8.1.16/ofi/gnu/9.1
+export CUDA_HOME=/soft/compilers/cudatoolkit/cuda-11.4.4/
 
-# export MPI_HOME=/opt/cray/pe/mpich/8.1.25/ofi/gnu/9.1
-# export CUDA_HOME=/opt/nvidia/hpc_sdk/Linux_x86_64/22.11/cuda  
-
-export MPI_HOME=/opt/cray/pe/mpich/8.1.25/ofi/nvidia/20.7
-export CUDA_HOME=/opt/nvidia/hpc_sdk/Linux_x86_64/22.11/cuda
+# export MPI_HOME=/opt/cray/pe/mpich/8.1.25/ofi/nvidia/20.7
+# export CUDA_HOME=/opt/nvidia/hpc_sdk/Linux_x86_64/22.11/cuda
 
 export MPIEXEC_HOME=/opt/cray/pe/pals/1.2.11
 export NCCL_NET_PLUGIN_HOME="/home/yuke/ncclPG/aws-ofi-nccl-1.7.4-aws/build"          
@@ -60,35 +57,35 @@ export LD_LIBRARY_PATH=${NCCL_NET_PLUGIN_HOME}/lib:${NCCL_SRC_LOCATION}/build/li
 
 $MPIEXEC_HOME/bin/mpiexec -n 2 --ppn 1 --cpu-bind core ${NCCL_TEST_HOME}/build/all_reduce_perf -b 2 -e 512MB -w 0 -n 1 -f 2 -g 1
 
-################################### NCCL TEST Profile ##########################################################
+# ################################### NCCL TEST Profile ##########################################################
 
-echo "NCCL TEST with NCCL Profile"
+# echo "NCCL TEST with NCCL Profile"
 
-export NCCL_TEST_PROFILE_HOME="/home/yuke/ncclPG/nccl-tests-profile"
+# export NCCL_TEST_PROFILE_HOME="/home/yuke/ncclPG/nccl-tests-profile"
 
-NCCL_PROFILE_SRC_LOCATION="/home/yuke/ncclPG/nccl_profile"
-export NCCL_PROFILE_SRC_LOCATION
+# NCCL_PROFILE_SRC_LOCATION="/home/yuke/ncclPG/nccl_profile"
+# export NCCL_PROFILE_SRC_LOCATION
 
-export LD_LIBRARY_PATH=${NCCL_NET_PLUGIN_HOME}/lib:${NCCL_PROFILE_SRC_LOCATION}/build/lib/:$LD_LIBRARY_PATH
+# export LD_LIBRARY_PATH=${NCCL_NET_PLUGIN_HOME}/lib:${NCCL_PROFILE_SRC_LOCATION}/build/lib/:$LD_LIBRARY_PATH
 
-$MPIEXEC_HOME/bin/mpiexec -n 2 --ppn 1 --cpu-bind core ${NCCL_TEST_PROFILE_HOME}/build/all_reduce_perf -b 2 -e 512MB -w 0 -n 1 -f 2 -g 1
+# $MPIEXEC_HOME/bin/mpiexec -n 2 --ppn 1 --cpu-bind core ${NCCL_TEST_PROFILE_HOME}/build/all_reduce_perf -b 2 -e 512MB -w 0 -n 1 -f 2 -g 1
 
-################################### MSCCL TEST #########################################################
+# ################################### MSCCL TEST #########################################################
 
-echo "NCCL TEST with MSCCL"
+# echo "NCCL TEST with MSCCL"
 
-export NCCL_TEST_MSCCL_HOME="/home/yuke/ncclPG/nccl-tests-msccl-test_profile"
-:q
-MSCCL_SRC_LOCATION="/home/yuke/ncclPG/msccl_test_profile"
-export MSCCL_SRC_LOCATION
+# export NCCL_TEST_MSCCL_HOME="/home/yuke/ncclPG/nccl-tests-msccl-test_profile"
+# :q
+# MSCCL_SRC_LOCATION="/home/yuke/ncclPG/msccl_test_profile"
+# export MSCCL_SRC_LOCATION
 
-export MSCCL_TOOLS_SRC_LOCATION="/home/yuke/ncclPG/msccl_tools_lyd"
+# export MSCCL_TOOLS_SRC_LOCATION="/home/yuke/ncclPG/msccl_tools_lyd"
 
-export LD_LIBRARY_PATH=${NCCL_NET_PLUGIN_HOME}/lib:${MSCCL_SRC_LOCATION}/build/lib/:$LD_LIBRARY_PATH
-export NCCL_DEBUG=TRACE
-export NCCL_DEBUG_SUBSYS=INIT,ENV
-export MSCCL_XML_FILES=${MSCCL_TOOLS_SRC_LOCATION}/examples/xml/allreduce_binary_tree_p_gpu01_2nodes_channel2_chunk4.xml
-export NCCL_ALGO=MSCCL,TREE,RING
-export NCCL_PROTO=Simple
+# export LD_LIBRARY_PATH=${NCCL_NET_PLUGIN_HOME}/lib:${MSCCL_SRC_LOCATION}/build/lib/:$LD_LIBRARY_PATH
+# export NCCL_DEBUG=TRACE
+# export NCCL_DEBUG_SUBSYS=INIT,ENV
+# export MSCCL_XML_FILES=${MSCCL_TOOLS_SRC_LOCATION}/examples/xml/allreduce_binary_tree_p_gpu01_2nodes_channel2_chunk4.xml
+# export NCCL_ALGO=MSCCL,TREE,RING
+# export NCCL_PROTO=Simple
 
-$MPIEXEC_HOME/bin/mpiexec -n 2 --ppn 1 --cpu-bind core ${NCCL_TEST_MSCCL_HOME}/build/all_reduce_perf -b 2 -e 512MB -w 0 -n 1 -f 2 -g 1
+# $MPIEXEC_HOME/bin/mpiexec -n 2 --ppn 1 --cpu-bind core ${NCCL_TEST_MSCCL_HOME}/build/all_reduce_perf -b 2 -e 512MB -w 0 -n 1 -f 2 -g 1
