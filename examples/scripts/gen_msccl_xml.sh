64711e07ccafee7e702b97517b9852b8e2327207
Liuyao DAi <liuyaodai@Liuyaos-Mac-mini.local>
2024-01-29 05:24:55 -0800

    4nodes 16gpus 2chunks

--- examples/scripts/gen_msccl_xml.sh
+++ examples/scripts/gen_msccl_xml.sh
@@ -374,4 +374,7 @@ export MSCCL_TOOLS_XML='/Users/liuyaodai/github/msccl_tools_lyd/examples/xml'
 # python3 ${MSCCL_TOOLS_ALGORITHMS}/ring/allreduce_a100_ring.py --protocol=Simple 128 128 1 > ${MSCCL_TOOLS_XML}/xml_lyd/ring/allreduce_basic_ring_32nodes_128gpus_128chunks_32channels_polaris.xml
 
 
-python3 ${MSCCL_TOOLS_ALGORITHMS}/binary/allreduce_binary_tree.py --protocol=Simple 8 2 1 > ${MSCCL_TOOLS_XML}/xml_lyd/allredcue_basic_binary_tree_8gpus.xml
+# python3 ${MSCCL_TOOLS_ALGORITHMS}/binary/allreduce_binary_tree.py --protocol=Simple 8 2 1 > ${MSCCL_TOOLS_XML}/xml_lyd/allredcue_basic_binary_tree_8gpus.xml
+
+
+python3 ${MSCCL_TOOLS_ALGORITHMS}/binary/allreduce_binary_tree_p_gpu01_channel_n_reverse.py --protocol=Simple 4 4 2 2 1 > ${MSCCL_TOOLS_XML}/xml_lyd/binary_h_gpu01_reverse_p/allreduce_binary_tree_p_gpu01_4nodes_16gpus_channel2_reverse_chunk2.xml
