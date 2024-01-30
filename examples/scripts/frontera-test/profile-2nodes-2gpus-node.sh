cbe0325699a5860b234fae2bd4c38851119e9d8e
Liuyao Dai <ldai1@c196-042.frontera.tacc.utexas.edu>
2024-01-29 22:18:19 -0600

    sendrecv frontera

--- examples/scripts/frontera-test/profile-2nodes-2gpus-node.sh
+++ examples/scripts/frontera-test/profile-2nodes-2gpus-node.sh
@@ -8,6 +8,25 @@ module load cuda/11.3
 export CUDA_HOME=/opt/apps/cuda/11.3
 export MPI_HOME=/scratch1/projects/compilers/intel18u5/compilers_and_libraries_2018.6.288/linux/mpi/intel64
 
+
+
+# ##################################### NCCL PROFILE #####################################
+# echo "##################################### NCCL PROFILE #####################################"
+# NCCL_PROFILE_SRC_LOCATION="/home1/09168/ldai1/ccl-build/NCCL_profile"
+# export NCCL_PROFILE_SRC_LOCATION
+
+# NCCLTESTS_NCCL_PROFILE_SRC_LOCATION="/home1/09168/ldai1/ccl-build/nccl-tests-profile"
+# export NCCLTESTS_NCCL_PROFILE_SRC_LOCATION
+
+# export LD_LIBRARY_PATH="${NCCL_PROFILE_SRC_LOCATION}/build/lib:${MPI_HOME}/lib:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH"
+
+# export NCCL_DEBUG=TRACE
+# export NCCL_ALGO=Tree
+# export NCCL_PROTO=Simple
+
+# $MPI_HOME/bin/mpirun -np 2 -ppn 2 $NCCLTESTS_NCCL_PROFILE_SRC_LOCATION/build/sendrecv_perf -b 1K -e 512MB -f 2 -g 1 -n 100 >> output.log 2>&1
+
+
 ##################################### NCCL #####################################
 echo "##################################### NCCL #####################################"
 NCCL_SRC_LOCATION="/home1/09168/ldai1/ccl-build/nccl"
@@ -23,5 +42,7 @@ export NCCL_PROTO=Simple
 export NCCL_NTHREADS=512
 
 $MPI_HOME/bin/mpirun -np 2 --hostfile /home1/09168/ldai1/ccl-build/msccl_tools_lyd/examples/scripts/frontera-test/myhostfile_2nodes -ppn 1 $NCCLTESTS_SRC_LOCATION/build/sendrecv_perf -b 1K -e 512MB -f 2 -g 1 -n 100 >> output.log 2>&1
+
+# $MPI_HOME/bin/mpirun -np 2 -ppn 2 $NCCLTESTS_SRC_LOCATION/build/sendrecv_perf -b 1K -e 512MB -f 2 -g 1 -n 100 >> output.log 2>&1
 
 
