28bed9bc91557c9e98c8adc7692058d090e987e6
Liuyao Dai <ldai1@login4.frontera.tacc.utexas.edu>
2024-03-28 20:54:04 -0500

    tree chunk-step 1 for frontera

--- examples/scripts/frontera-test/sc24/script/allreduce_binary_tree_4.sh
+++ examples/scripts/frontera-test/sc24/script/allreduce_binary_tree_4.sh
@@ -7,7 +7,7 @@
 #SBATCH -N 4               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 16               # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -t 01:00:00        # Run time (hh:mm:ss)
-##SBATCH --exclude=c199-121,c199-051,c197-022,c199-012
+#SBATCH --exclude=c197-072
 ##SBATCH --mail-type=all    # Send email at begin and end of job
 ##SBATCH -A ccl-run-4nodes-16gpus       # Project/Allocation name (req'd if you have more than 1)
 ##SBATCH --mail-user=username@tacc.utexas.edu
@@ -20,7 +20,7 @@ module load cuda/11.3
 
 
 export CUDA_HOME=/opt/apps/cuda/11.3
-export MPI_HOME=/scratch1/projects/compilers/intel18u5/compilers_and_libraries_2018.6.288/linux/mpi/intel64
+export MPI_HOME=/opt/intel/compilers_and_libraries_2019.5.281/linux/mpi/intel64
 
 ##################################### MSCCL #####################################
 echo "##################################### MSCCL #####################################"
@@ -56,8 +56,8 @@ for nnodes in "${nodes_values[@]}"; do
             for trees in "${trees_values[@]}"; do
                 echo "Running MSCCL tree test with ${nnodes} nodes, ${nchannel} channels, ${nchunks} chunks, ${trees} trees"
                 export MSCCL_XML_FILES=${MSCCL_TOOLS_XML}/binary_tree/allreduce_binary_tree_${nchannel}ch_${trees}tree_${nchunks}chunk_${nnodes}node_$((nnodes*ngpus))gpu.xml
-                ibrun -n $((nnodes*ngpus)) --ntasks-per-node=$ngpus --cpu-bind core $NCCLTESTS_MSCCL_SRC_LOCATION/build/all_reduce_perf -b 64K -e 256MB -f 2 -g 1 -n 60 \
-                > /home1/09168/ldai1/ccl-build/msccl_tools_lyd/examples/scripts/frontera-test/sc24/log/paper0/chunk_step_2/all-reduce_sum_float_binary-tree_node${nnodes}_gpu$((nnodes*ngpus))_mcl${nchannel}_mck${nchunks}_i0.out
+                ibrun -n $((nnodes*ngpus)) --ntasks-per-node=$ngpus $NCCLTESTS_MSCCL_SRC_LOCATION/build/all_reduce_perf -b 64K -e 256MB -f 2 -g 1 -n 60 \
+                > /home1/09168/ldai1/ccl-build/msccl_tools_lyd/examples/scripts/frontera-test/sc24/log/paper0/chunk_step_1/tree/all-reduce_sum_float_binary-tree_node${nnodes}_gpu$((nnodes*ngpus))_mcl${nchannel}_mck${nchunks}_i0.out
             done
         done
     done
