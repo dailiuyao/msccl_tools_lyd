5d212cb903213fe5038a19a42e6abdc533f5a930
Liuyao DAi <liuyaodai@Liuyaos-Mac-mini.local>
2024-04-01 02:02:14 -0700

    nothing important

--- examples/scripts/polaris-test/sc24/log/paper0/format_converter.sh
+++ examples/scripts/polaris-test/sc24/log/paper0/format_converter.sh
@@ -1,13 +1,44 @@
 #!/bin/bash
 
-# Directory containing the files to process
-directory="/home/liuyao/scratch/deps/msccl_tools_lyd/examples/scripts/polaris-test/sc24/log/paper0/buffer_size_1"
+# # Directory containing the files to process
+# directory="/home/liuyao/scratch/deps/msccl_tools_lyd/examples/scripts/polaris-test/sc24/log/paper0/buffer_size_1"
 
-# Loop through each file in the directory
+# # Loop through each file in the directory
+# for file in "$directory"/*; do
+#   echo "Processing $file..."
+#   # Use sed to find and replace all instances within each file
+#   sed -i -r 's/allreduce_binary_tree_([0-9]+)ch_([0-9]+)tree_([0-9]+)chunk_([0-9]+)node_([0-9]+)gpu.xml/allreduce_binary-tree_node\4_gpu\5_mcl\1_mck\3_gan0.xml/g' "$file"
+# done
+
+# echo "Processing complete."
+
+# directory="/Users/liuyaodai/github/msccl_tools_lyd/examples/scripts/polaris-test/sc24/log/paper0/buffer_size_4/tree"
+
+# for file in "$directory"/*; do
+#     mv "$file" "${file//i0.out/buf4_gan0_i0.log}"
+# done
+
+# echo "Processing complete."
+
+directory="/Users/liuyaodai/github/msccl_tools_lyd/examples/scripts/polaris-test/sc24/log/paper0/buffer_size_4/tree"
+
+dst_directory="/Users/liuyaodai/github/sc24-log/polaris"
+
 for file in "$directory"/*; do
-  echo "Processing $file..."
-  # Use sed to find and replace all instances within each file
-  sed -i -r 's/allreduce_binary_tree_([0-9]+)ch_([0-9]+)tree_([0-9]+)chunk_([0-9]+)node_([0-9]+)gpu.xml/allreduce_binary-tree_node\4_gpu\5_mcl\1_mck\3_gan0.xml/g' "$file"
+    cp "$file" "$dst_directory/$(basename "$file")"
 done
 
 echo "Processing complete."
+
+
+# all-reduce_sum_float_binary-tree_node4_gpu16_mcl1_mck1_i0
+
+# all-reduce_binary-tree_node4_gpu32_mcl16_mck1_buf1_gan0_i0.log
+
+# directory="/Users/liuyaodai/github/msccl_tools_lyd/examples/scripts/polaris-test/sc24/log/paper0/buffer_size_4/ring"
+
+# for file in "$directory"/*; do
+#     cp "$file" "${file//node32_gpu128/node64_gpu256}"
+# done
+
+
