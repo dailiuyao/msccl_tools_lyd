0bbfd9188da1ef9a7fa9fab8ae57d020c49992f6
dailiuyao <ldai8@ucmerced.edu>
2024-05-15 11:05:55 -0700

    pingpong inter script v4

--- examples/scripts/ncclguage/polaris/gauge-run-inter.sh
+++ examples/scripts/ncclguage/polaris/gauge-run-inter.sh
@@ -55,15 +55,22 @@ export LD_LIBRARY_PATH=${NCCL_SRC_LOCATION}/build/lib:${MPI_HOME}/lib:${CUDA_HOM
 export NCCL_GAUGE_HOME="/home/yuke/ncclPG/CCL-LYD/msccl_tools_lyd/examples/scripts/ncclguage"
 export GAUGE_OUT_DIRE="/home/yuke/ncclPG/CCL-LYD/msccl_tools_lyd/examples/scripts/ncclguage/polaris"
 export GAUGE_HEO="inter"
-export GAUGE_MODE="pingpong"
 export GAUGE_CHUNK_SIZE="2"
+
 
 for ((itr = 0; itr < 5; itr += 1)); do
     for ((nch = 1; nch <= 4; nch *= 2)); do
-        export GAUGE_ITERATION=${itr} 
-        export GAUGE_NCHANNELS=${nch}
-        export NCCL_MIN_NCHANNELS=${nch}
-        export NCCL_MAX_NCHANNELS=${nch}
-        $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/gauge.exe
+        for mode in pping ppong; do
+            for ((n = 1; n <= 32; itr *= 2)); do
+                for ((msize=64; msize<=256*1024; msize*=2)); do
+                    export GAUGE_MESSAGE_SIZE=${msize}
+                    export GAUGE_ITERATION=${itr} 
+                    export GAUGE_NCHANNELS=${nch}
+                    export GAUGE_MODE=${mode}
+                    export NCCL_MIN_NCHANNELS=${nch}
+                    export NCCL_MAX_NCHANNELS=${nch}
+                    $MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/${mode}_gauge_${n}.exe
+            done
+        done
     done 
 done
