af2bf9368026e98e6edc05e527b0bf6693b379ca
dailiuyao <ldai8@ucmerced.edu>
2024-05-14 20:37:20 -0700

    run build sbatch for polaris v2

--- examples/scripts/ncclguage/polaris/gauge-run.sh
+++ examples/scripts/ncclguage/polaris/gauge-run.sh
@@ -52,7 +52,7 @@ export LD_LIBRARY_PATH=${NCCL_SRC_LOCATION}/build/lib:${MPI_HOME}/lib:${CUDA_HOM
 
 # export NCCL_NTHREADS=256
 
-export NCCL_GAUGE_HOME = "/home/yuke/ncclPG/CCL-LYD/msccl_tools_lyd/examples/scripts/ncclguage/gauge"
+export NCCL_GAUGE_HOME = "/home/yuke/ncclPG/CCL-LYD/msccl_tools_lyd/examples/scripts/ncclguage"
 
 # Compilation command. Ensure to link against the MPI and NCCL libraries correctly.
 # nvcc $NVCC_GENCODE -ccbin g++ -I${NCCL_SRC_LOCATION}/build/include -I${MPI_HOME}/include -L${NCCL_SRC_LOCATION}/build/lib -L${CUDA_HOME}/lib64 -L${MPI_HOME}/lib -lnccl -lcudart -lmpi $1 -o ${1%.cu}.exe
@@ -66,6 +66,6 @@ else
     echo "Compilation failed."
 fi
 
-$MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge.exe 
+$MPIEXEC_HOME/bin/mpirun -n 2 --ppn 1 --cpu-bind core $NCCL_GAUGE_HOME/gauge/gauge.exe 
 
 
